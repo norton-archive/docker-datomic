a160f56468e3488b4bf96190d7ea007d9aef8c3c
Joseph Wayne Norton <norton@alum.mit.edu>
2016-04-19 00:57:07 +0900

    Upgrade Clojure deps

--- build/datomic-tester/jepsen.datomic/project.clj
+++ build/datomic-tester/jepsen.datomic/project.clj
@@ -5,12 +5,12 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-exec "0.3.6"]]
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/tools.nrepl "0.2.10"]
-                 [clojure-complete/clojure-complete "0.2.3"]
+                 [org.clojure/tools.nrepl "0.2.12"]
+                 [clojure-complete/clojure-complete "0.2.4"]
                  [com.datomic/datomic-pro "0.9.5350"
                   :exclusions [org.slf4j/slf4j-nop org.slf4j/log4j-over-slf4j]]
-                 [org.slf4j/slf4j-log4j12 "1.6.4"]
-                 [org.postgresql/postgresql "9.3-1102-jdbc41"]
-                 [org.apache.httpcomponents/httpclient "4.3.5"]
-                 [clj-http "2.1.0"]
-                 [jepsen "0.0.9"]])
+                 [org.slf4j/slf4j-log4j12 "1.7.21"]
+                 [org.postgresql/postgresql "9.4.1208"]
+                 [org.apache.httpcomponents/httpclient "4.5.2"]
+                 [clj-http "3.0.1"]
+                 [jepsen "0.1.0"]])
